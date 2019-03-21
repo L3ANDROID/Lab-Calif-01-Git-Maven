79f4c04acfc9cfc5ab57fa43b9e2a4c20f47fc39
L3ANDROID <40245178+L3ANDROID@users.noreply.github.com>
2019-03-21 13:23:40 -0500

    clase UserController listo

--- src/main/java/com/tecsup/lab01/controller/UserController.java
+++ src/main/java/com/tecsup/lab01/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.tecsup.lab01.controller;
+import com.tecsup.lab01.model.User;
+import javax.swing.JOptionPane;
 
-/**
- * Hello world!
- *
- */
-public class UserController 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class UserController {
+	static String nombre,apellido,edad;
+	
+    public static void main( String[] args ){
+    	User u=new User();
+        
+        nombre=u.nombre;
+        apellido=u.apellido;
+        edad=u.edad;
+    }
+    public String datos(){
+    	String data=nombre+" "+apellido+" con edad de "+edad+" años";
+    	return data;
     }
 }
